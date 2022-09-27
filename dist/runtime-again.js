0cadc2d7f75084d2c6a9f1876f5b3bab3a983bf9
Yeferson Mejias <yefer.el.teke@gmail.com>
2022-09-26 23:52:33 -0500

    chore: remove dist

--- dist/runtime-again.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.start = void 0;
-function start() {
-    console.log("start");
-}
-exports.start = start;
-//# sourceMappingURL=runtime-again.js.map
